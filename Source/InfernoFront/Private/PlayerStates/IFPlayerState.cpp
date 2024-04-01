34a1699702970b7ba4c1e0a9c9313efb5ff444e7
wandin <wanderson.lopes@outlook.com>
2024-04-01 20:40:24 -0300

    Fixed - AbilitySystem Object creation causing crash

--- Source/InfernoFront/Private/PlayerStates/IFPlayerState.cpp
+++ Source/InfernoFront/Private/PlayerStates/IFPlayerState.cpp
@@ -4,12 +4,14 @@
 #include "PlayerStates/IFPlayerState.h"
 
 #include "AbilitySystemComponent.h"
+
+#include "AbilitySystem/AbilitySystemComponents/IFAbilitySystemComponent.h"
 #include "AbilitySystem/AttributeSets/IFAttributeSet.h"
 
 AIFPlayerState::AIFPlayerState()
 {
 	// Create and initialize the ability system component
-	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent = CreateDefaultSubobject<UIFAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
 	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 
